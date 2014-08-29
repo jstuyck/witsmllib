1f8e649cff83d023c4a719da3601e8a7f63a8e85
Julien Stuyck <julien.stuyck@techdrill.com>
2014-08-29 15:55:39 +0100

    Add WitsmlWellboreGeometry and WitsmlWellboreGeometrySection

---
+++ witsmllib/WitsmlWellboreGeometry.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace witsmllib
+{
+    /// <summary>
+    /// Captures information about the configuration of the permanently
+    /// installed components in a wellbore. It does not define the transient
+    /// drilling strings (see the tubular data-object) or the hanging
+    /// production components. This data-object is uniquely identified
+    /// within the context of one wellbore data-object.
+    /// </summary>
+    public abstract class WitsmlWellboreGeometry : WitsmlObject
+    {
+        private static String WITSML_TYPE = "wbGeometry";
+
+        private DateTime? _dTimReport;     //Time report generated.
+        private Value _mdBottom;           //Measured depth at bottom.
+        private Value _gapAir;             //Air gap.
+        private Value _depthWaterMean;     //Water depth.
+        private WitsmlWellboreGeometrySection _wbGeometrySection; //Wellbore geometry section object.
+
+        protected WitsmlWellboreGeometry(WitsmlServer server,
+                                      String id, String name,
+                                      WitsmlObject parent, String parentId)
+
+            : base(server, WITSML_TYPE, id, name, parent, parentId)
+        { }
+
+        /// <summary>
+        /// Get the Time when the report was generated.
+        /// </summary>
+        protected DateTime? DTimReport
+        {
+            get { return _dTimReport; }
+        }
+
+        /// <summary>
+        /// Get the measured depth at bottom.
+        /// </summary>
+        public Value MdBottom
+        {
+            get { return _mdBottom; }
+        }
+
+        /// <summary>
+        /// Get the Air gap.
+        /// </summary>
+        public Value GepAir
+        {
+            get { return _gapAir; }
+        }
+
+        /// <summary>
+        /// Get the water depth.
+        /// </summary>
+        public Value DepthWaterMean
+        {
+            get { return _depthWaterMean; }
+        }
+
+        /// <summary>
+        /// Get the Wellbore geometry section object.
+        /// </summary>
+        public WitsmlWellboreGeometrySection WbGeometrySection
+        {
+            get { return _wbGeometrySection; }
+        }
+
+
+     
+    }
+}
---
+++ witsmllib/WitsmlWellboreGeometrySection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace witsmllib
+{
+    class WitsmlWellboreGeometrySection : WitsmlObject
+    {
+        private static String WITSML_TYPE = "wbGeometrySection";
+
+
+        protected String typeHoleCasing;    //Type of fixed component. ENUM
+
+        protected Value mdTop;              //Measured depth at Top of Interval.
+        protected Value mdBottom;           //Measured depth at bottom of the section.
+        protected Value tvdTop;             //True vertical depth at top of the section.
+        protected Value tvdBottom;          //True vertical depth at bottom of the section.
+        protected Value idSection;          //Inner diameter.
+        protected Value odSection;          //Outer diameter - Only for casings and risers.
+        protected Value wtPerLen;           //Weight per unit length for casing sections.
+        protected String grade;             //Material grade for the tubular section.
+
+        protected Boolean? curveConductor;  //Curved conductor? Values are "true" (or "1") and "false" (or "0").
+        protected Value diaDrift;           //Maximum diameter that can pass through.
+        protected Value factFric;           //Friction factor.
+        protected Object extensionNameValue;//cs_extensionNameValue
+
+        protected WitsmlWellboreGeometrySection(WitsmlServer server,
+                                                String id, String name,
+                                                WitsmlObject parent, String parentId)
+
+            : base(server, WITSML_TYPE, id, name, parent, parentId)
+        { }
+
+    }
+}
