7420971836e020e83166303958480cb4f0699f31
Julien Stuyck <julien.stuyck@techdrill.com>
2014-09-01 16:51:06 +0100

    change Target Object

--- witsmllib/WitsmlTarget.cs
+++ witsmllib/WitsmlTarget.cs
@@ -1,43 +1,37 @@
-/*
-nwitsml Copyright 2010 Setiri LLC
-Derived from the jwitsml project, Copyright 2010 Statoil ASA
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+using System;
 
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 namespace witsmllib
 {
 
-    /**
-     * Java representation of a WITSML "target".
-     *
-     * @author <a href="mailto:info@nwitsml.org">NWitsml</a>
-     */
-    using System;
+    
+   
     public abstract class WitsmlTarget : WitsmlObject
     {
 
-        /** The WITSML type name */
+        
         private  static String WITSML_TYPE = "target";
 
         protected String parentTargetId; // uidTargetParent
         protected Value north; // dispNsCenter
         protected Value east; // dispEwCenter
         protected Value tvd; // tvd
+        protected Value dispNsOffset;           //North-south offset of target intercept point from shape center.
+        protected Value dispEwOffset;           //East-west offset of target intercept point from shape center.
+        protected Value thickAbove;             //Height of target above center point.
+        protected Value thickBelow;             //Depth of target below center point.
+        protected Value dip;                    //Angle of dip with respect to horizontal.
 
-        /**
-         * Create a well object with specified ID.
-         *
-         * @param id  ID of this well.
-         */
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="server"></param>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="parent"></param>
+        /// <param name="parentId"></param>
         protected WitsmlTarget(WitsmlServer server, String id, String name,
                                WitsmlObject parent, String parentId)
             : base(server, WITSML_TYPE, id, name, parent, parentId)
@@ -62,5 +56,36 @@ namespace witsmllib
         {
             return tvd;
         }
+
+
+
+        /// <summary>
+        /// Return the north south offset of this target. 
+        /// </summary>
+        /// <returns>The north south offset of this target. May be null if absent or unknown.</returns>
+        public Value getNorthSouthOffset()
+        {
+            return dispNsOffset;
+        }
+
+        /// <summary>
+        /// Return the east west offset of this target.
+        /// </summary>
+        /// <returns>The east west offset of this target. May be null if absent or unknown.</returns>
+        public Value getEastWestOffset()
+        {
+            return dispEwOffset;
+        }
+
+        /// <summary>
+        /// Return the dip of this target. 
+        /// </summary>
+        /// <returns>The dip of this target. May be null if absent or unknown.</returns>
+        public Value getDip()
+        {
+            return dip;
+        }
+
+
     }
 }
