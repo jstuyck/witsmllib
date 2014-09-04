e60ecb46c966219bc3eadffd3845335c336d79f8
Julien Stuyck <julien.stuyck@techdrill.com>
2014-09-04 17:08:32 +0100

    Clean FunctionCapability and Value

--- witsmllib/FunctionCapability.cs
+++ witsmllib/FunctionCapability.cs
@@ -1,104 +1,86 @@
-/*
-nwitsml Copyright 2010 Setiri LLC
-Derived from the jwitsml project, Copyright 2010 Statoil ASA
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-namespace witsmllib{
-
-//import java.util.ArrayList;
-//import java.util.Collection;
-//import java.util.Collections;
-
-/**
- * Model the object capabilities for a given WITSML function.
- * An example would be the "WMLS_GetFromStore" function where the
- * objects could be "well", "wellbore" and "log" etc.
- *
- * @author <a href="mailto:info@nwitsml.org">NWitsml</a>
- */
 using System;
 using System.Text;
 using System.Collections.Generic;
-sealed class FunctionCapability {
-    /** Name of function. */
-    private  String functionName;
 
-    /** Version. */
-    private  String version;
+namespace witsmllib
+{
 
-    /** Objects supported for this function. */
-    private  List<String> supportedWitsmlTypes = new List<String>();
+    /// <summary>
+    /// Model the object capabilities for a given WITSML function. 
+    /// Note that the client program does not provide function capabilities.
+    /// </summary>
 
-    /**
-     * Create a new function capability object for the function
-     * with the specified name.
-     *
-     * @param name  Name of the function.
-     */
-    internal FunctionCapability(String functionName, String version) {
-        //Debug.Assert(functionName != null : "functionName cannot be null";
+    sealed class FunctionCapability
+    {
+       
+        private String functionName;
+        private String version;      
+        private List<String> supportedWitsmlTypes = new List<String>();
 
-        this.functionName = functionName;
-        this.version = version;
+        /// <summary>
+        /// Create a new function capability object for the function
+        /// with the specified name.
+        /// </summary>
+        /// <param name="functionName">Name of the function</param>
+        /// <param name="version">Version of the function</param>
+        internal FunctionCapability(String functionName, String version)
+        {
+            if (functionName == null)
+                throw new ArgumentNullException("functionName cannot be null");
+       
+            this.functionName = functionName;
+            this.version = version;
+        }
+
+        /// <summary>
+        /// Return name of the function (such as "WMLS_GetFromStore" etc.).
+        /// </summary>
+        /// <returns>The name of the function.</returns>
+        internal String getFunctionName()
+        {
+            return functionName;
+        }
+
+        /// <summary>
+        /// Return WITSML version this capability is valid for. 
+        /// </summary>
+        /// <returns>the WITSML version this capability is valid for. </returns>
+        internal String getVersion()
+        {
+            return version;
+        }
+
+        /**
+         * Add data object supported by this function. Data object is
+         * a valid WITSML data type name.
+         *
+         * @param dataObject  Data object supported by this function.
+         */
+        internal void addWitsmlType(String witsmlType)
+        {
+            supportedWitsmlTypes.Add(witsmlType);
+        }
+
+        /// <summary>
+        /// Return the WITSML types (such as "well", "wellbore", "log" etc.) supported by this function. 
+        /// </summary>
+        /// <returns>WITSML types supported by this function. Never null.</returns>
+        internal List<String> getSupportedWitsmlTypes()
+        {
+            return supportedWitsmlTypes;
+        }
+
+        /** {@inheritDoc} */
+
+        public override String ToString()
+        {
+            StringBuilder s = new StringBuilder(functionName);
+            s.Append("(");
+            foreach (String witsmlType in supportedWitsmlTypes)
+                s.Append(witsmlType + " ");
+            s.Append(")");
+            return s.ToString();
+        }
     }
-
-    /**
-     * Return name of the function (such as "WMLS_GetFromStore" etc.)
-     *
-     * @return  Name of the function. Never null.
-     */
-    internal String getFunctionName() {
-        return functionName;
-    }
-
-    /**
-     * Return WITSML version this capability is valid for.
-     *
-     * @return  WITSML version this capability is valid for. May be null.
-     */
-    internal String getVersion() {
-        return version;
-    }
-
-    /**
-     * Add data object supported by this function. Data object is
-     * a valid WITSML data type name.
-     *
-     * @param dataObject  Data object supported by this function.
-     */
-    internal void addWitsmlType(String witsmlType) {
-        supportedWitsmlTypes.Add(witsmlType);
-    }
-
-    /**
-     * Return the WITSML types (such as "well", "wellbore", "log" etc.)
-     * supported by this function.
-     *
-     * @return  WITSML types supported by this function. Never null.
-     */
-    internal List<String> getSupportedWitsmlTypes() {
-        return supportedWitsmlTypes; // Collections.unmodifiableCollection(supportedWitsmlTypes);
-    }
-
-    /** {@inheritDoc} */
-    
-    public override String ToString() {
-        StringBuilder s = new StringBuilder(functionName);
-        s.Append("(");
-        foreach (String witsmlType in supportedWitsmlTypes)
-            s.Append(witsmlType + " ");
-        s.Append(")");
-        return s.ToString();
-    }
-}
 }
--- witsmllib/Value.cs
+++ witsmllib/Value.cs
@@ -1,92 +1,70 @@
-/*
-nwitsml Copyright 2010 Setiri LLC
-Derived from the jwitsml project, Copyright 2010 Statoil ASA
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 using System;
-namespace witsmllib{
 
-/**
- * Model a property value with a corresponding unit.
- *
- * @author <a href="mailto:info@nwitsml.org">NWitsml</a>
- */
-public sealed class Value : IComparable<Value> { // Comparable<Value> {
+namespace witsmllib
+{
 
-    /** Value of this instance. May be null to indicate absent. */
-    private  Double? value;
+    public sealed class Value : IComparable<Value>
+    {
+        private Double? value;
+        private String unit;
 
-    /** Unit of this instance. May be null indicating unitless or unknown. */
-    private  String unit;
+        /// <summary>
+        /// Create a new immutable value with unit instance. 
+        /// </summary>
+        /// <param name="value">Value of this instance. May be null to indicate absent.</param>
+        /// <param name="unit">Unit of this instance. May be null to indicate unitless or unknown.</param>
+        public Value(Double value, String unit)
+        {
+            this.value = value;
+            this.unit = unit;
+        }
 
-    /**
-     * Create a new immutable value with unit instance.
-     *
-     * @param value  Value of this instance.
-     *               May be null to indicate absent.
-     * @param unit   Unit of this instance. May be null to indicate
-     *               unitless or unknown.
-     */
-    public Value(Double value, String unit) {
-        this.value = value;
-        this.unit = unit;
+        /// <summary>
+        /// Return value of this instance. 
+        /// </summary>
+        /// <returns>Value of this instance. May be null indicating absent.</returns>
+        public Double? getValue()
+        {
+            return value;
+        }
+
+        
+        /// <summary>
+        /// Return unit of this instance. 
+        /// </summary>
+        /// <returns>Unit of this instance. May be null indicating unitless or unknown.</returns>
+        public String getUnit()
+        {
+            return unit;
+        }
+
+        /**
+         * Compare this value to the specified value. Ordering follows
+         * the natural order of the number of the instances. Instances
+         * of null value are ordered after those without null.
+         *
+         * @param v  Value to compare to. Non-null.
+         * @return   A negative integer, zero, or a positive integer as
+         *           this object is less than,
+         *           equal to, or greater than the specified object.
+         * @throws IllegalArgumentEAxception  If v is null.
+         */
+        public int CompareTo(Value v)
+        {
+            Double v1 = value.HasValue ? value.Value : Double.MaxValue; //.MAX_VALUE;
+            Double v2 = v.value.HasValue ? v.value.Value : Double.MaxValue; //.MAX_VALUE;
+
+            return v1.CompareTo(v2); //.compareTo(v2);
+        }
+
+       
+        /// <summary>
+        /// Return a string representation of this instance. 
+        /// </summary>
+        /// <returns>A string representation of this instance. Never null.</returns>
+        public override String ToString()
+        {
+            return value + " [" + unit + "]";
+        }
     }
-
-    /**
-     * Return value of this instance.
-     *
-     * @return  Value of this instance. May be null indicating absent.
-     */
-    public Double? getValue() {
-        return value;
-    }
-
-    /**
-     * Return unit of this instance.
-     *
-     * @return  Unit of this instance. May be null indicating unitless
-     *          or unknown.
-     */
-    public String getUnit() {
-        return unit;
-    }
-
-    /**
-     * Compare this value to the specified value. Ordering follows
-     * the natural order of the number of the instances. Instances
-     * of null value are ordered after those without null.
-     *
-     * @param v  Value to compare to. Non-null.
-     * @return   A negative integer, zero, or a positive integer as
-     *           this object is less than,
-     *           equal to, or greater than the specified object.
-     * @throws IllegalArgumentEAxception  If v is null.
-     */
-    public int CompareTo(Value v) {
-        Double v1 = value.HasValue ? value.Value  : Double.MaxValue; //.MAX_VALUE;
-        Double v2 = v.value.HasValue ? v.value.Value  : Double.MaxValue ; //.MAX_VALUE;
-
-        return v1.CompareTo(v2 ); //.compareTo(v2);
-    }
-
-    /**
-     * Return a string representation of this instance.
-     *
-     * @return  A string representation of this instance. Never null.
-     */
-    
-    public override String ToString() {
-        return value + " [" + unit + "]";
-    }
-}
 }
